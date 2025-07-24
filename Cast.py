08adf416efe40e9a0e08a0a78315ce4d2d6e6682
EduardoJDLC <eduardodelaconch@gmail.com>
2025-07-23 19:47:40 -0600

    Add files via upload
    Archivo donde se aprende cast o variables

---
+++ Cast.py
@@ -0,0 +1,38 @@
+numero = "147"
+print(numero)
+print(type(numero))
+
+numero = int(numero)
+print(numero)
+print(type(numero))
+
+numero = float(numero)
+print(numero)
+print(type(numero))
+
+numero = complex(numero)
+print(numero)
+print(type(numero))
+
+zapato = 0
+print(zapato)
+print(type(zapato))
+zapato = bool(zapato)
+print(zapato)
+print(type(zapato))
+
+numero_uno = 0
+numero_dos = 1
+
+x = 9
+X = 7
+print(X)
+
+numeroUno = 0
+numeroDos = 1
+
+numerouno = 0 
+numerodos = 1
+
+help("keywords")
+
