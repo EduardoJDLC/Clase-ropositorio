f97a02b835b9a152eb3466aad2ca17d4dd41b390
EduardoJDLC <eduardodelaconch@gmail.com>
2025-07-23 19:52:07 -0600

    Add files via upload
    Operadores Python

---
+++ Operadores.py
@@ -0,0 +1,39 @@
+# Operadores aritmeticos
+print(2 + 2) #suma
+print(4 - 1) #resta
+print(4 * 8) #multiplicacion
+print(4 / 2) #division
+print(11 % 4) #modulo
+print(11 // 4) #division de piso
+print(2 ** 3) #potenciación
+
+# Operadores en cadenas
+print("Hola " + "mundo") #Concatenación
+print("Hola " * 3) #repetición
+print("Hola " + "mundo" * 3) #Concatenación y repetción
+
+#operadores de comparación
+print("Hola" == "hola") #igual que
+print("Hola" != "hola") #distinto que
+print(3 > 44) #mayor que, mwenor que
+print(11 >= 10) #mayor o igual que
+print(10 <= 10) #menor o igual que
+
+# Operadores de existencia
+print("ola" in "Hola") # De existencia
+print("ola" not in "Hola") # De inexistencia
+
+# Operadores booleanos
+print(True or False) #or verifica que se cumpla una de las dfos condiciones
+print(True and False) #and verifica que se cumplan las dos condiciones
+
+#Operadores de asignación
+x = 1 # Operador de asignacioón  estandar
+x += 2 # Operador de asignacion suma
+x *= 3 # Operador de asignación multiplicación
+x %= 4 # Operador de asignación módulo
+print (x)
+
+x = 6 * 5 + 8 / 2 ** 4
+print(x)
+
