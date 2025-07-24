63fa99f42cb81d08ed8f9e2d1a28f266a1113753
EduardoJDLC <eduardodelaconch@gmail.com>
2025-07-23 19:52:24 -0600

    Add files via upload
    Variables python

---
+++ Variables.py
@@ -0,0 +1,25 @@
+# Aqui hay una variable de tipo entero
+numero = 147
+print(numero)
+print(type(numero))
+#Aqui hay una variable de tipo punto decimal
+numero = 147.7
+print(numero)
+print(type(numero))
+
+complejo = 1 + 2j
+print(complejo)
+print(type(complejo))
+
+palabra = "Hola"
+print(palabra)
+print(type(palabra))
+
+zapato = True
+print(zapato)
+print(type(zapato))
+
+jose = None
+print(jose)
+print(type(jose))
+
