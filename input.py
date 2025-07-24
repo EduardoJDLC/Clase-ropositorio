e9d0d981376ec2cd16bc036ad62e77110e6631db
EduardoJDLC <eduardodelaconch@gmail.com>
2025-07-23 19:51:46 -0600

    Add files via upload
    input

---
+++ input.py
@@ -0,0 +1,14 @@
+# nombre = input("¿Como te llamas? ")
+# print("Hola " + nombre)
+
+# edad = input("¿Cuantos años tienes? ")
+# print(type(edad))
+# print(f"{nombre} tiene {edad} años")
+
+# #Programa que pida dos numeros al usuario y los suma
+
+numero1 = int(input("Introduce un número por favor: "))
+numero2 = int(input("Introduce otro número porfavor: "))
+numero3 = numero1 + numero2 
+
+print(f"El resultado de la suma es {numero3}")
