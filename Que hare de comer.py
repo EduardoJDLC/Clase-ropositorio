96f692c488fbc7a117314b8c9804797e9e272bd5
EduardoJDLC <eduardodelaconch@gmail.com>
2025-08-06 20:18:03 -0600

    Add files via upload

---
+++ Que hare de comer.py
@@ -0,0 +1,12 @@
+#Qué hare de comer mañana
+print("¿Que hare el dia de mañana?")
+comida = input()
+if comida == "Pechuga asada":
+    print("Usar sal y pimienta")
+elif comida == "Pescado al vapor":
+    print("Usar hierbas finas, sal y pimienta")
+else:
+    comida == "Tacos dorados de pollo"
+    print("Usar aceite para dorar los tacos")
+    
+    
